3b389f3ff2bfaf8f790bc1592f99f2849ece4897
genesis-bernadin <112427891+genesis-bernadin@users.noreply.github.com>
2022-10-04 21:52:26 -0400

    HTML validate corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 
 /* javascript */
 console.log("Reimagined concert poster by designer Mike Joyce for his Swissted project and reproduced by Genesis Bernadin for her DIG247 project")
+window.scrollTo({
+  top: 0
+});
